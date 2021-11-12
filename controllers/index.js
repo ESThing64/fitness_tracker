ae0097144faa964494f0155f2772e00a9911a692
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-11 20:12:00 -0600

    fixed the routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,26 +2,27 @@ const express = require("express");
 const router = require('express').Router();
 const { Exercise, Days} = require('../models');
 const db = require("../models");
+const path = require("path");
 
 
 
 router.get("/", (req, res) => {
-    res.render(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname + '/public/index.html'));
   });
 
  
 router.get("/stats", (req, res) => {
-    res.render(__dirname + '/public/stats.html');
+    res.sendFile(path.join(__dirname + '../../public/stats.html'));
   });
 
  
 router.get("/exercise", (req, res) => {
-    res.render(__dirname + '/public/exercise.html');
+    res.sendFile(path.join(__dirname + '../../public/exercise.html'));
   });
 
  
 router.get("/exercise?", (req, res) => {
-    res.render(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname + '../../public/exercise.html'));
   });
 
  
