ed98f4b039ca2453b024a2090e97158ccf08a236
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-11-11 22:19:39 -0600

    setup the workout Dashboard charts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -15,17 +15,25 @@ router.post("/workouts", ({body}, res) => {
   
 
 router.get("/workouts/range", ({body}, res) => {
-    db.Workout.find({})
-    .populate("workouts")
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+    db.Workout.aggregate([{
+        $addFields:{
+            totalDuration: { $sum: "$exercises.duration"}
+        }
+    }])
+    
+    .then(results => {
+        console.log(results)
+      res.json(results);
     })
     .catch(err => {
       res.json(err);
     });
 });
 
+
   
+
+// *Important:** Look into using a MongoDB aggregate function to dynamically add up and return the total duration for each workout. Check out the [MongoDB documentation on the $addFields](https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/), the [MongoDB documentation on the $sum operator](https://docs.mongodb.com/manual/reference/operator/aggregation/sum/), and the [Mongoose documentation on aggregate functions](https://mongoosejs.com/docs/api.html#aggregate_Aggregate) to learn how it can be accomplished.
 
 
   module.exports = router;
